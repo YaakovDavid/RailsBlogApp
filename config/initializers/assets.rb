7dc6cbb4d0c7165eb513ef35054a688362571c8c
yaakov <ykvdvd@gmail.com>
2018-08-22 02:23:27 -0400

    work!

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,4 @@
 # Be sure to restart your server when you modify this file.
-RAILS_ENV=production bundle exec rake assets:precompile
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
