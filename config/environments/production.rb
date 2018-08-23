2374239e7992a5096ea3d7b331153d380fef243e
yaakov <ykvdvd@gmail.com>
2018-08-23 13:42:10 -0400

    maybe this time

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,13 +83,24 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-config.paperclip_defaults = {
+# config.paperclip_defaults = {
+#   storage: :s3,
+#   s3_credentials: {
+#     bucket: ENV('S3_BUCKET_NAME'),
+#     access_key_id: ENV('AWS_ACCESS_KEY_ID'),
+#     secret_access_key: ENV('AWS_SECRET_ACCESS_KEY'),
+#     s3_region: ENV('AWS_REGION'),
+#   }
+# }
+
+
+ config.paperclip_defaults = {
   storage: :s3,
-  s3_credentials: {
-    bucket: ENV('S3_BUCKET_NAME'),
-    access_key_id: ENV('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV('AWS_REGION'),
+  s3_credentials:  {
+    bucket:  ENV['S3_BUCKET_NAME'],
+    access_key_id:  ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
+    s3_region: ENV['AWS_REGION']
   }
 }
 
